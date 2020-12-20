6804317accb4eff2e5aa04d9709d2b198d3eec8f
Eren Gedik <67486777+erenged1k@users.noreply.github.com>
2020-12-20 08:18:01 +0300

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,57 @@
+fx_version 'adamant' 
+
+game 'gta5' 
+
+version '1.0.0' 
+
+--[[
+********************************************************************
+CONTACT: EREN GEDIK
+********************************************************************
+Github: https://github.com/erenged1k
+Discord: erenged1k#3369
+Discord Server: https://discord.gg/3t5m6MJ
+********************************************************************
+]]
+
+files {
+    'index.html',
+	'timeout.html',
+    'music/loadscreen.mp3',
+    'img/loading.gif',
+	'img/loadscreen.jpg',
+	'img/logo2.png',
+	'img/bg-1.jpg',
+    'img/bg-2.jpg',
+	'img/bg-3.jpg',
+	'img/bg-4.jpg',
+	'img/bg-5.jpg',
+	'img/bg-6.jpg',
+	'img/bg-7.jpg',
+	'img/bg-8.jpg',
+	'img/bg-9.jpg',
+	'img/bg-10.jpg',
+	'img/bg-11.jpg',
+	'img/bg-12.jpg',
+	'img/bg-13.jpg',
+	'img/bg-14.jpg',
+	'img/bg-15.jpg',
+	'img/bg-16.jpg',
+	'img/bg-17.jpg',
+	'img/bg-18.jpg',
+	'img/bg-19.jpg',
+	'img/bg-20.jpg',
+	'img/logo.png',
+    'img/timeout.png',
+	'css/bootstrap.css',
+    'css/owl.carousel.css',
+	'css/style.css',
+    'js/jquery.ajaxchimp.js',
+	'js/jquery.backstretch.min.js',
+    'js/jquery-1.11.0.min.js',
+	'js/lj-safety-first.js',
+    'js/owl.carousel.min.js',
+}
+
+loadscreen 'index.html'
+
